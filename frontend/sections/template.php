649bc1d84debc8ff41491e01c0fa7b965a516163
EkoAlex <alexandru@appticles.com>
2018-06-28 16:35:26 +0300

    Fix new template

--- frontend/sections/template.php
+++ frontend/sections/template.php
@@ -1,166 +1,107 @@
 <?php
+use  PWAPP\Frontend\Application;
 
-	use \PWAPP\Frontend\Application;
-	use \PWAPP\Inc\Themes_Config;
-	use \PWAPP\Inc\Options;
+global $pwapp;
 
-	$app_settings = Application::load_app_settings();
+$frontend = new Application();
 
-	$frontend_path = plugins_url() . '/' . PWAPP_DOMAIN . '/frontend/';
-	$theme_path    = $frontend_path . 'themes/app2/';
+$app_settings = $frontend->load_app_settings();
 
-	// check fonts
-	$loaded_fonts = array(
-		$app_settings['font_headlines'],
-		$app_settings['font_subtitles'],
-		$app_settings['font_paragraphs'],
-	);
+$site_name = get_bloginfo( 'name' );
 
-	$loaded_fonts = array_unique( $loaded_fonts );
+$site_url = get_site_url();
 
-	// check if locale file exists
-	$texts_json_exists = Application::check_language_file( get_locale() );
+$theme_path = plugins_url() . '/' . PWAPP_DOMAIN . '/frontend/themes/app2/';
 
-	if ( false === $texts_json_exists ) {
-		echo "ERROR, unable to load language file. Please check the '" . PWAPP_DOMAIN . "/frontend/locales/' folder.";
-	}
+$config = [
+	'export'      => [
+		'categories' => $site_url . '/wp-json/pwapp/categories/',
+		'posts'      => $site_url . '/wp-json/wp/v2/posts/',
+		'pages'      => $site_url . '/wp-json/wp/v2/pages?_embed=media',
+		'comments'   => $site_url . '/wp-json/wp/v2/comments',
+		'media'      => $site_url . '/wp-json/wp/v2/media',
+	],
+	'translate'   => [
+		'path' => $site_url . '/wp-json/pwapp/language/',
+	],
+	'socialMedia' => [
+		'facebook' => $app_settings['enable_facebook'],
+		'twitter'  => $app_settings['enable_twitter'],
+		'google'   => $app_settings['enable_google'],
+	],
+	'websiteUrl'  => home_url() . parse_url( home_url(), PHP_URL_QUERY ) ? '&' : '?' . Options::$prefix . 'theme_mode=desktop',
+	'ga-id'       => 'UA-000000-01',
+	'googleAds'   => [
+		'adsInterval' => '10',
+		'phone'       => [
+			'networkCode' => '1060237',
+			'adUnitCode'  => 'dev.demo.ad.test1',
+			'sizes'       => [
+				[
+					336,
+					280,
+				],
+				[
+					300,
+					300,
+				],
+				[
+					300,
+					250,
+				],
+				[
+					250,
+					250,
+				],
+			],
+		],
+	],
 
-	$background_color = Themes_Config::get_manifest_background( $app_settings['color_scheme'] );
-	?>
-<!DOCTYPE HTML>
-<html manifest="" <?php language_attributes(); ?>>
+];
+
+$config_json = wp_json_encode( $config );
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
 <head>
-	<meta charset="UTF-8" />
-	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no" />
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,minimum-scale=1,user-scalable=no" />
 	<meta name="apple-mobile-web-app-capable" content="yes" />
 	<meta name="apple-touch-fullscreen" content="yes" />
 	<meta name="apple-mobile-web-app-status-bar-style" content="black" />
-	<link rel="apple-touch-icon-precomposed" href="" />
 	<meta name="mobile-web-app-capable" content="yes" />
-	<link rel="manifest" href="<?php echo $frontend_path . 'export/content.php?content=androidmanifest'; ?>" />
-
-	<?php if ( false !== $background_color ) : ?>
-		<meta name="theme-color" content="<?php echo $background_color; ?>">
+	<meta name="theme-color" content="#a333c8">
+	<link rel="manifest" href="<?php echo  $site_url . '/wp-json/pwapp/manifest/'; ?>">
+	<?php if ( '' != $app_settings['icon'] ) : ?>
+		<link rel="apple-touch-icon" href="<?php echo $app_settings['icon']; ?>" />
 	<?php endif; ?>
-
-	<?php if ( '' != $app_settings['icon'] ) : // icon path for Firefox ?>
-		<link rel="shortcut icon" href="<?php echo $app_settings['icon']; ?>"/>
-	<?php endif; ?>
-
-	<title><?php echo get_bloginfo( 'name' ); ?></title>
-	<noscript>Your browser does not support JavaScript!</noscript>
-	<style type="text/css">
-		/**
-		* Example of an initial loading indicator.
-		* It is recommended to keep this as minimal as possible to provide instant feedback
-		* while other resources are still being loaded for the first time
-		*/
-		html, body {
-			height: 100%;
-			width: 100%;
-			margin: 0;
-			padding: 0;
-			background-color: #e5e8e3;
-		}
-
-		#appLoadingIndicator {
-			position: absolute;
-			top: 50%;
-			margin-top: -8px;
-			text-align: center;
-			width: 100%;
-			height: 16px;
-			-webkit-animation-name: appLoadingIndicator;
-			-webkit-animation-duration: 0.5s;
-			-webkit-animation-iteration-count: infinite;
-			-webkit-animation-direction: linear;
-			animation-name: appLoadingIndicator;
-			animation-duration: 0.5s;
-			animation-iteration-count: infinite;
-			animation-direction: linear;
-		}
-
-		#appLoadingIndicator > * {
-			background-color: #c6cdbe;
-			display: inline-block;
-			height: 16px;
-			width: 16px;
-			-webkit-border-radius: 8px;
-			-moz-border-radius: 8px;
-			border-radius: 8px;
-			margin: 0 2px;
-			opacity: 0.8;
-		}
-
-		@-webkit-keyframes appLoadingIndicator{
-			0% {
-				opacity: 0.8
-			}
-			50% {
-				opacity: 0
-			}
-			100% {
-				opacity: 0.8
-			}
-		}
-
-		@keyframes appLoadingIndicator{
-			0% {
-				opacity: 0.8
-			}
-			50% {
-				opacity: 0
-			}
-			100% {
-				opacity: 0.8
-			}
-		}
-	</style>
-
+	<link rel="shortcut icon" href="/favicon.ico">
+	<title><?php echo esc_html( $site_name ); ?></title>
+	<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css">
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"/>
 	<script type="text/javascript" pagespeed_no_defer="">
-		var appticles = {
-			exportPath: "<?php echo $frontend_path . 'export/'; ?>",
+		window.__INITIAL_CONFIG__ =  <?php echo $config_json; ?>
+	</script>
+	<link href="<?php echo $theme_path; ?>css/app.css" rel="stylesheet">
+</head>
 
-			websiteUrl: '<?php echo home_url(); echo parse_url( home_url(), PHP_URL_QUERY ) ? '&' : '?'; echo Options::$prefix; ?>theme_mode=desktop',
-			logo: "<?php echo $app_settings['logo']; ?>",
-			icon: "<?php echo $app_settings['icon']; ?>",
-			defaultCover: "<?php echo  '' != $app_settings['cover'] ? $app_settings['cover'] : $frontend_path . 'images/pattern-' . rand( 1, 6 ) . '.jpg'; ?>",
-			userCover: <?php echo intval( '' != $app_settings['cover'] ); ?>,
-			hasFacebook: <?php echo $app_settings['enable_facebook']; ?>,
-			hasTwitter: <?php echo $app_settings['enable_twitter']; ?>,
-			hasGoogle: <?php echo $app_settings['enable_google']; ?>,
-			commentsToken: "<?php echo $app_settings['comments_token']; ?>"
+<body>
+	<noscript>You need to enable JavaScript to run this app.</noscript>
+	<div id="root" style="height:100%"></div>
+	<script type="text/javascript" src="<?php echo $theme_path; ?>js/app.js"></script>
+</body>
+
+<?php if ( '1' === $app_settings['service_worker_installed'] ) : ?>
+	<script>
+		if ('serviceWorker' in navigator) {
+			navigator.serviceWorker.register('/sw.js');
 		}
 	</script>
+<?php endif; ?>
 
-
-	<!-- core -->
-	<?php if ( '' != $app_settings['theme_timestamp'] ) : ?>
-		<link rel="stylesheet" href="<?php echo PWAPP_FILES_UPLOADS_URL . 'theme-' . $app_settings['theme_timestamp'] . '.css'; ?>" type="text/css" />
-	<?php else : ?>
-		<link rel="stylesheet" href="<?php echo $theme_path; ?>css/phone.css?date=20160420" type="text/css" />
-	<?php endif; ?>
-
-	<!-- custom fonts -->
-	<?php foreach ( $loaded_fonts as $font_no ) : ?>
-		<link rel="stylesheet" href="<?php echo $frontend_path . 'fonts/font-' . $font_no . '.css?date=20160106'; ?>" type="text/css">
-	<?php endforeach; ?>
-
-	<script src="<?php echo $frontend_path . 'export/content.php?content=apptexts&locale=' . get_locale(); ?>" type="text/javascript"></script>
-	<script src="<?php echo $theme_path; ?>js/app.js?date=20160525" type="text/javascript"></script>
-	<?php if ( 1 == $app_settings['service_worker_installed'] ) : ?>
-		<script type="text/javascript" pagespeed_no_defer="">
-			if ('serviceWorker' in navigator) {
-				 navigator.serviceWorker.register('/sw.js');
-			 }
-		 </script>
-	<?php endif; ?>
-</head>
-<body>
-<div id="appLoadingIndicator">
-	<div></div>
-	<div></div>
-	<div></div>
-</div>
-</body>
 </html>
